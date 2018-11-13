1da1fa31c7132e409b5775567cba854874ab6701
dekeli <liwenzheng3701@163.com>
2018-11-13 10:44:59 +0800

    Update your_service.py

--- api_auto/api_call/your_service.py
+++ api_auto/api_call/your_service.py
@@ -19,16 +19,18 @@ class NbService(BaseHttp):
         url = '57ce4182905e887f7b8b456c/api/channel/followers'
         response = self.get_re(url=url, params=request_params)
         return response
-
-    def get_your_request_2(self, request_params):
+    
+    #其他传值方式    
+    def get_your_request_2(self, key_1=None，key_2=None,key_3=None):
+        
         """
         GET /v1/your/request
         获取配置
         请求参数：request_params(假如需要传get请求的参数)
         :return: 无
         """
-        url = '/search_api/'
-        response = self.get_re(url=url, params=request_params)
+        url = '{key_1}/api/channel/followers'.format(key_1=key_1)
+        response = self.get_re(url=url, params={"key_2":key_2,"key_3":key_3})
         return response
 
     def post_your_request(self, data):
